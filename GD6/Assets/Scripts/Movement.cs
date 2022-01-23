e870098d5b51fc93a0466e577fe806a349128a43
Nicolas Hammar <nico.hammar@gmail.com>
2022-01-23 22:08:22 +0100

    The hand now slaps. It slaps for too long, you can slap with both at at the same time and you can cue the end of your next slap before your first slap is over, but it slaps.

--- GD6/Assets/Scripts/Movement.cs
+++ GD6/Assets/Scripts/Movement.cs
@@ -4,15 +4,65 @@ using UnityEngine;
 
 public class Movement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public GameObject LeftSlap;
+    public GameObject RightSlap;
+    public GameObject Hand;
+
+    void SlapLeft()
     {
-        
+        Hand.SetActive(false);
+        LeftSlap.SetActive(true);
+        Debug.Log("0");
+        StartCoroutine(Wait(1));
+    }
+
+    void SlapLeftDone()
+    {
+        Debug.Log("1");
+        Hand.SetActive(true);
+        LeftSlap.SetActive(false);
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SlapLeftDone();
+    }
+
+    void SlapRight()
+    {
+        Hand.SetActive(false);
+        RightSlap.SetActive(true);
+        Debug.Log("0");
+        StartCoroutine(Waiting(1));
+    }
+
+    void SlapRightDone()
+    {
+        Debug.Log("1");
+        Hand.SetActive(true);
+        RightSlap.SetActive(false);
+    }
+
+    IEnumerator Waiting(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        SlapRightDone();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown("left"))
+        {
+            SlapLeft();
+            Debug.Log("Left");
+        }
+
+        if (Input.GetKeyDown("right"))
+        {
+            SlapRight();
+            Debug.Log("Right");
+        }
     }
 }
