41aab7122b7a42c4852ecafc26b35c50d75c5fa5
Nicolas Hammar <nico.hammar@gmail.com>
2022-01-25 14:32:36 +0100

    Slap now slaps good

--- GD6/Assets/Scripts/Movement.cs
+++ GD6/Assets/Scripts/Movement.cs
@@ -7,25 +7,29 @@ public class Movement : MonoBehaviour
     public GameObject LeftSlap;
     public GameObject RightSlap;
     public GameObject Hand;
+    public bool Slapping = false;
+    public float Cooldown = 0.25f;
 
     void SlapLeft()
     {
         Hand.SetActive(false);
         LeftSlap.SetActive(true);
+        Slapping = true;
         Debug.Log("0");
-        StartCoroutine(Wait(1));
+        StartCoroutine(Wait());
     }
 
     void SlapLeftDone()
     {
         Debug.Log("1");
+        Slapping = false;
         Hand.SetActive(true);
         LeftSlap.SetActive(false);
     }
 
-    IEnumerator Wait(float seconds)
+    IEnumerator Wait()
     {
-        yield return new WaitForSeconds(seconds);
+        yield return new WaitForSeconds(Cooldown);
         SlapLeftDone();
     }
 
@@ -33,20 +37,22 @@ public class Movement : MonoBehaviour
     {
         Hand.SetActive(false);
         RightSlap.SetActive(true);
+        Slapping = true;
         Debug.Log("0");
-        StartCoroutine(Waiting(1));
+        StartCoroutine(Waiting());
     }
 
     void SlapRightDone()
     {
         Debug.Log("1");
+        Slapping = false;
         Hand.SetActive(true);
         RightSlap.SetActive(false);
     }
 
-    IEnumerator Waiting(float seconds)
+    IEnumerator Waiting()
     {
-        yield return new WaitForSeconds(seconds);
+        yield return new WaitForSeconds(Cooldown);
         SlapRightDone();
     }
 
@@ -55,12 +61,14 @@ public class Movement : MonoBehaviour
     {
         if (Input.GetKeyDown("left"))
         {
+            if (Slapping == false)
             SlapLeft();
             Debug.Log("Left");
         }
 
         if (Input.GetKeyDown("right"))
         {
+            if (Slapping == false)
             SlapRight();
             Debug.Log("Right");
         }
