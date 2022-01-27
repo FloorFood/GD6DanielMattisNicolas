a9baa78e854cc104df9809a43da8b5db6deb902a
Nicolas Hammar <nico.hammar@gmail.com>
2022-01-27 01:39:29 +0100

    Just made a spawner object

---
+++ GD6/Assets/Scripts/Ghostspawner.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ghostspawner : MonoBehaviour
+{
+    public GameObject LeftGhost;
+    public GameObject RightGhost;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
