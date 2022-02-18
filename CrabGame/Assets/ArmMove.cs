d8d4fc6c3663a5dcc1d0d63683192c14805d1989
Nicolas Hammar <nico.hammar@gmail.com>
2022-02-18 17:06:08 +0100

    Made a test scene and tested some stuff. There is an error with the arm movement script.

---
+++ CrabGame/Assets/ArmMove.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ArmMove : MonoBehaviour
+{
+    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
