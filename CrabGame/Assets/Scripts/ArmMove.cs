ecb103b5427461f5123aa944c07774750976739e
Nicolas Hammar <nico.hammar@gmail.com>
2022-02-20 18:19:01 +0100

    Arms now follow the mouse, but they are not rotating around the right part of the sprite.

--- CrabGame/Assets/Scripts/ArmMove.cs
+++ CrabGame/Assets/Scripts/ArmMove.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class ArmMove : MonoBehaviour
 {
-    Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+    
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +15,9 @@ public class ArmMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector2 direction = mousePosition - transform.position;
+        float angle = Vector2.SignedAngle(Vector2.right, direction);
+        transform.eulerAngles = new Vector3(0, 0, angle);
     }
 }
