70b550a3d771b337881d785f55b89c55f86c833b
Nicolas Hammar <nico.hammar@gmail.com>
2022-02-22 12:17:16 +0100

    Can now grab only leego.

--- CrabGame/Assets/ClawScript.cs
+++ CrabGame/Assets/ClawScript.cs
@@ -4,24 +4,38 @@ using UnityEngine;
 
 public class ClawScript : MonoBehaviour
 {
-
+    public GameObject Arm;
+    private Collider2D ArmCollider;
     bool LeftClick = false;
     //bool RightClick = false;
-    private void OnTriggerStay2D(Collision2D collision2D)
+    void OnTriggerStay2D(Collider2D collision2D)
     {
         if (LeftClick == true)
         {
-            Debug.Log("HoldLeft");
-            //attach collided object to this object
+            if (collision2D.tag == "leego")
+            {
+                Debug.Log("HoldLeft");
+                ArmCollider.enabled = false;
+                collision2D.attachedRigidbody.transform.position = gameObject.transform.position;
+            }
+        }
+        else
+        {
+            ArmCollider.enabled = true;
         }
     }
 
+    private void Start()
+    {
+        ArmCollider = Arm.GetComponent<Collider2D>();
+    }
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             LeftClick = true;
+            //pinch sound
         }
 
         if (Input.GetMouseButtonUp(0))
