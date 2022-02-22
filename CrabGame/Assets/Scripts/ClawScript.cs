23e860f4815dfabb8cf08d8c80d325afdd886ee4
Nicolas Hammar <nico.hammar@gmail.com>
2022-02-22 16:59:34 +0100

    Crab can now only grab one leego at a time and the whole thing is less buggy.

--- CrabGame/Assets/Scripts/ClawScript.cs
+++ CrabGame/Assets/Scripts/ClawScript.cs
@@ -5,29 +5,53 @@ using UnityEngine;
 public class ClawScript : MonoBehaviour
 {
     public GameObject Arm;
+    public GameObject OtherArm;
     private Collider2D ArmCollider;
+    private Collider2D OtherArmCollider;
     bool LeftClick = false;
-    //bool RightClick = false;
+    int Leegos = 0;
+
     void OnTriggerStay2D(Collider2D collision2D)
     {
-        if (LeftClick == true)
+        if (LeftClick && Leegos == 1)
         {
             if (collision2D.tag == "leego")
             {
-                Debug.Log("HoldLeft");
                 ArmCollider.enabled = false;
+                OtherArmCollider.enabled = false;
+                collision2D.attachedRigidbody.freezeRotation = true;
                 collision2D.attachedRigidbody.transform.position = gameObject.transform.position;
             }
         }
         else
         {
-            ArmCollider.enabled = true;
+            if (collision2D.tag == "leego")
+            {
+                collision2D.attachedRigidbody.freezeRotation = false;
+            }
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "leego")
+        {
+            Leegos = Leegos + 1;
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "leego")
+        {
+            Leegos = Leegos - 1;
         }
     }
 
     private void Start()
     {
         ArmCollider = Arm.GetComponent<Collider2D>();
+        OtherArmCollider = OtherArm.GetComponent<Collider2D>();
     }
     // Update is called once per frame
     void Update()
@@ -41,6 +65,8 @@ public class ClawScript : MonoBehaviour
         if (Input.GetMouseButtonUp(0))
         {
             LeftClick = false;
+            ArmCollider.enabled = true;
+            OtherArmCollider.enabled = true;
         }
     }
 }
