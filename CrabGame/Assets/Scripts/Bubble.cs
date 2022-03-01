934d0fafb990f188d16a3d2387d03c0a6798303d
Daniel Cehic <danielcehic26@gmail.com>
2022-03-01 15:58:43 +0100

    Did bubbly things
    -Added some temporary files and a scene specifically for testing this stuff
    -Added BubbleSpawner and Bubble prefab. BubbleSpawner creates bubbles at its position at random intervals, and those bubbles expires after 8 seconds.
    -Added sounds for bubbles, crab footsteps, crab snapping and an underwater ambience

---
+++ CrabGame/Assets/Scripts/Bubble.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bubble : MonoBehaviour
+{
+    private float deathCountdown = 8.0f; //Does this one need an explonation?
+    private float speed = 2.2f; //How fast the bubble goes up
+    private float frequency = 7.0f; //How fast the bubble swings
+    private float magnitude = 0.2f; //How much the bubble swings
+
+    private Vector3 axis;
+    private Vector3 pos;
+
+    void Start()
+    {
+        pos = transform.position;
+        axis = transform.right;
+    }
+
+    void Update()
+    {
+        pos += transform.up * Time.deltaTime * speed;
+        transform.position = pos + axis * Mathf.Sin(Time.time * frequency) * magnitude;
+
+        deathCountdown -= 0.01f;
+        if (deathCountdown <= 0.0f)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
