84b9cc738c67686fc686731fb40746da360a58e9
Nicolas Hammar <nico.hammar@gmail.com>
2022-03-02 17:21:01 +0100

    screenshot works

--- CrabGame/Assets/Scripts/RightClawScript.cs
+++ CrabGame/Assets/Scripts/RightClawScript.cs
@@ -88,17 +88,25 @@ public class RightClawScript : MonoBehaviour
     public void Screenshot()
     {
         //possibly animate the button going down.
-        //play camera sound and play a flash.
+        //play camera beep sound and maybe animate a flash.
         RightClick = false;
-        Debug.Log("Screenshot");
-        ScreenCapture.CaptureScreenshot("BuiltByCrab");
         StartCoroutine(Ending());
     }
 
     IEnumerator Ending()
     {
         yield return new WaitForSeconds(5);
+        //play camera shutter sound.
+        Debug.Log("Buttonpressed");
+        TakeScreenshot();
+
+        yield return new WaitForSeconds(2);
         Debug.Log("end");
         Application.Quit();
+    }
+
+    public void TakeScreenshot()
+    {
+        ScreenCapture.CaptureScreenshot("BuiltByCrab.png");
     }
 }
