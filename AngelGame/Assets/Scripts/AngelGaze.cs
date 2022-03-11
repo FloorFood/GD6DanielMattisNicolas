1e2f9164d992940638d1ee4ec9b0b4ec9f7bc2fd
Nicolas Hammar <nico.hammar@gmail.com>
2022-03-11 17:31:04 +0100

    Not much, just want to grab the new main scene.

---
+++ AngelGame/Assets/Scripts/AngelGaze.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AngelGaze : MonoBehaviour
+{
+    public GameObject Look1;
+    public GameObject Look2;
+    public GameObject Look3;
+    public GameObject Look4;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
