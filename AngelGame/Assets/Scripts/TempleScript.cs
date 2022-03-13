0f206c1ce6b2ee588584642cea87c5a341cf5719
Nicolas Hammar <nico.hammar@gmail.com>
2022-03-13 20:35:30 +0100

    The winscreen now displays the amount of sinners you saved and lost.

--- AngelGame/Assets/Scripts/TempleScript.cs
+++ AngelGame/Assets/Scripts/TempleScript.cs
@@ -1,14 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TempleScript : MonoBehaviour
 {
     public GameObject Endscreen;
     public GameObject WinScreen;
     public GameObject Player;
+    public GameObject SavedText;
+    public GameObject LostText;
+
+    Text AmountSaved;
+    Text AmountLost;
 
     public int Sinners = 6;
+    public int Saved = 0;
+    public int Lost = 0;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
@@ -22,6 +30,7 @@ public class TempleScript : MonoBehaviour
             //You save the sinner.
             Destroy(collision.gameObject);
             ReduceSinner();
+            IncreaseSaved();
         }
     }
 
@@ -29,6 +38,23 @@ public class TempleScript : MonoBehaviour
     {
         Sinners = Sinners - 1;
     }
+
+    void IncreaseSaved()
+    {
+        Saved = Saved + 1;
+    }
+
+    public void IncreaseLost()
+    {
+        Lost = Lost + 1;
+    }
+
+    void Start()
+    {
+        AmountSaved = SavedText.GetComponent<Text>();
+        AmountLost = LostText.GetComponent<Text>();
+    }
+
     void Update()
     {
         
@@ -36,6 +62,8 @@ public class TempleScript : MonoBehaviour
         {
             Player.SetActive(false);
             WinScreen.SetActive(true);
+            AmountSaved.text = Saved.ToString();
+            AmountLost.text = Lost.ToString();
         }
     }
 }
